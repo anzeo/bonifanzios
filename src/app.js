e80bfdca2834c9fd427879f734a5e75dee3be06b
anzeo <zebigbozz@gmail.com>
2015-06-29 17:56:23 +0200

    Added dynamic title

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 angular.module("sbf", ["ngMaterial", "ui.router"], function($stateProvider, $urlRouterProvider){
 	$stateProvider.state("players", {
 		url: "/players",
-		templateUrl: "src/views/players.html"
+		templateUrl: "src/views/players.html",
+		data: {
+			title: 'Spelers'
+		}
 	}).state("scores", {
 		url: "/scores",
 		controller: "ScoresController",
 		controllerAs: "vm",
-		templateUrl: "src/views/scores.html"
+		templateUrl: "src/views/scores.html",
+		data: {
+			title: 'Uitslagen'
+		}
 	});
 
 	$urlRouterProvider.when("/", "/players");
+}).run(function($rootScope, $state){
+	$rootScope.$state = $state;
 });
